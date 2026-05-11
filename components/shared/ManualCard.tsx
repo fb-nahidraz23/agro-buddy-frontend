e82b538667d9eb5c6ab29f9d514272715ee8694a
nahidraz23 <nzamanraz@gmail.com>
2026-05-11 17:25:28 +0600

    fix: props added for manual card

--- components/shared/ManualCard.tsx
+++ components/shared/ManualCard.tsx
@@ -2,7 +2,13 @@ import { Trees } from "lucide-react"
 import React from "react"
 import CardTitle from "./ManualCardTitle"
 
-const ManualCard = ({ icon, title, desc }) => {
+interface ManualCardProps {
+  icon: React.ReactNode
+  title: string
+  desc: string
+}
+
+const ManualCard = ({ icon, title, desc }: ManualCardProps) => {
   return (
     <div className="flex flex-col items-center gap-4 rounded-2xl border border-card-border bg-card-background px-2 py-6">
       <div className="">{icon}</div>
